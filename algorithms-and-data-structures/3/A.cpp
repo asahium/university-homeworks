64a7acaf7f5621f6f6f6462726ee23235c5e902f
frank drebin <67472342+asahium@users.noreply.github.com>
2021-09-19 14:44:05 +0300

    some hw

--- algorithms-and-data-structures/3/A.cpp
+++ algorithms-and-data-structures/3/A.cpp
@@ -1,89 +1,70 @@
-#include <iostream>
-#include <vector>
 
-bool gen(int k, int s, std::vector<std::vector<int>> v) {
-    int i = 0;
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> generateHammingCode(
+        vector<int> msgBits, int m, int r) {
+    vector<int> hammingCode(r + m);
+
+    for (int i = 0; i < r; ++i) {
+        hammingCode[pow(2, i) - 1] = -1;
+    }
+
     int j = 0;
-    int CurS = 0;
-    for (size_t i = 1; i < k; ++i) {
-        for (size_t j = 0; j < k; ++j) {
-            if (CurS < s) {
-                v[i][j] = 1;
-                CurS += 1;
-            }
+
+    for (int i = 0; i < (r + m); i++) {
+        if (hammingCode[i] != -1) {
+            hammingCode[i] = msgBits[j];
+            j++;
         }
     }
+
+    for (int i = 0; i < (r + m); i++) {
+        if (hammingCode[i] != -1)
+            continue;
+
+        int x = log2(i + 1);
+        int one_count = 0;
+        for (int j = i + 2;
+             j <= (r + m); ++j) {
+
+            if (j & (1 << x)) {
+                if (hammingCode[j - 1] == 1) {
+                    one_count++;
+                }
+            }
+        }
+        if (one_count % 2 == 0) {
+            hammingCode[i] = 0;
+        }
+        else {
+            hammingCode[i] = 1;
+        }
+    }
+    return hammingCode;
+}
+
+void findHammingCode(vector<int>& msgBit) {
+    int m = msgBit.size();
+    int r = 1;
+    while (pow(2, r) < (m + r + 1)) {
+        r++;
+    }
+    vector<int> ans
+            = generateHammingCode(msgBit, m, r);
+    cout << "Message bits are: ";
+    for (int i = 0; i < msgBit.size(); i++)
+        cout << msgBit[i] << " ";
+
+    cout << "\nHamming code is: ";
+    for (int i = 0; i < ans.size(); i++)
+        cout << ans[i] << " ";
 }
 
 int main() {
-    int n, k, s;
-    std::cin >> n >> k >> s;
-    std::vector<std::vector<int>> v;
-    if (gen(k, s, v)) {
+    vector<int> msgBit = { 1, 0, 1, 0 };
 
-    }
+    findHammingCode(msgBit);
+
     return 0;
 }
-
-
-/*
-
-
-const MaxK=100;
-
-type
-
-Square = array [1..MaxK, 1..MaxK] of byte;
-{квадрат KxK, повторением которого получаем ответ}
-
-function gen(k, s : word; var a : Square) : boolean;
-
-var
-
-i, j : integer;
-
-CurS : word; { сколько единиц уже удалось поставить в квадрате KxK }
-
-Begin
-
-CurS:=0;
-
-fillchar(a, sizeof(a), 0); {вначале заполняем квадрат нулями}
-
-for i := 1 to k do
-
-for j := 1 to k do
-
-if CurS < s then {если число поставленных единиц меньше необходимого}
-
-begin
-
-a[i, j] := 1; {то ставим очередную единицу}
-
-inc(CurS);
-
-end
-
-end;
-
-if CurS < s then gen := false else gen := true;
-
-end;
-
-
-
-
- if gen(k, s, a) then {генерируем образец}
-
-for i := 1 to n do {заполняем образцом квадрат размером N*N}
-
-begin {результат выводим сразу в файл}
-
-for j := 1 to n do
-
-write(a[(i-1) mod k+1,(j-1) mod k+1], ' ');
-
-writeln;
-
-end;
-*/
