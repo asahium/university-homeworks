030f60aa6fb2661cfab8b01ff4a0783003a978c0
asahium <psiwyrm@tuta.io>
2023-03-01 18:58:03 +0200

    edit

--- caos/CAOS/13-4.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    unsigned long long size = 0;
-    unsigned int l, r;
-    while (scanf("%x-%x %*[^\n]%*c", &l, &r) == 2) {
-        size += r - l;
-    }
-    printf("%llu", size);
-}
--- caos/caos2021/sm06/1/submissions/02857-OK.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(void)
-{
-    unsigned int a, b;
-    scanf("%u%u", &a, &b);
-    unsigned int average = (a / 2) + (b / 2) + (a % 2) * (b % 2); 
-    printf("%u\n", average);
-    return 0;
-}
