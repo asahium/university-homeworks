97bd40cb5cfe26baa788baaf01be631f1c1a4e32
asahium <doodwyrm@protonmail.com>
2023-07-05 00:10:18 +0300

    some nup updates

---
+++ NUP/Web app technologies/Term Assignment/database.php
@@ -0,0 +1,20 @@
+<?php
+// Database configuration
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database_name";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Perform database operations (e.g., SELECT, INSERT, UPDATE, DELETE)
+
+// Close connection
+$conn->close();
+?>
