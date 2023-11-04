8a271880111b42c2f0a655631b28e2e8c98abdc1
asahium <doodwyrm@protonmail.com>
2023-11-04 18:14:29 +0200

    asasd

--- NUP/Networks/lab3/src2/tcp.py
+++ NUP/Networks/lab3/src2/tcp.py
@@ -3,7 +3,6 @@ import socket
 from struct import pack
 from time import sleep
 
-# 
 # |--------------------|--------------------|
 # |     SOURCE PORT    |   DESTINATION PORT |
 # |--------------------|--------------------|
@@ -15,33 +14,26 @@ from time import sleep
 # |--------------------|--------------------|
 # |    Checksum        |    URGENT POINTER  |
 # |--------------------|--------------------|
-# 
-# 
 
 def ip_header(src, dst):
     return pack("!BBHHHBBH4s4s", 69, 0, 0, 1, 0, 64, 6, 0, socket.inet_aton(src), socket.inet_aton(dst))
 
-# pack function allows you to pack different values (bytes, shorts, ints, strings ) into bytes
-# Format:
-# ! - network byte order
-# B unsigned char
-# H unsigned short
-# I unsigned int
-# L unsigned long
-# Q unsigned long long
-# s char[]
-
-my_ip = '10.10.10.99' # change to your ip
+my_ip = '10.10.11.181' # change to your IP
 target_ip = '10.10.10.65'
-# Warning: you need r00t to run this
-# Good luck (:
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
 sock.setsockopt(socket.SOL_IP, socket.IP_HDRINCL, 1)
 ip_hdr = ip_header(my_ip, target_ip)
-# TODO - fix this 2 variables
-tcp_hdr = b'00000000000000000000'
+
+# Define the TCP RST packet
+source_port = 1337
+destination_port = 7331
+sequence_number = 321342
+acknowledgment_number = 81321
 data = b''
-# /TODO
+
+# Create the TCP header
+tcp_hdr = pack('!HHLLBBHHH', source_port, destination_port, sequence_number, acknowledgment_number, 0, 0x04, 0, 0, 0)
 
 while True:
     sock.sendto(ip_hdr + tcp_hdr + data, (target_ip, 0))
